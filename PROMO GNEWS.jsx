c1ee7dbcf3a8af0e6238d43c6987816d577d2278
jmbillard <jmbillard2@gmail.com>
2022-04-12 22:04:53 -0300

    -

--- PROMO GNEWS.jsx
+++ PROMO GNEWS.jsx
@@ -32,7 +32,7 @@ if ( ExternalObject.AdobeXMPScript == undefined ) {
 function PROMO_GNEWS_UTL(thisObj) {
 
   // current script version...
-  var vStr = 'v 1.6';
+  var vStr = 'v RAFA';
   
   // app script network access preferences...
   var prefSection = 'Main Pref Section';
