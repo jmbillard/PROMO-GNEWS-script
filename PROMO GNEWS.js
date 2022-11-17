6dbe012d144380cd0a8eb3eae5340dd52a725310
jmbillard <jmbillard2@gmail.com>
2022-11-17 14:14:05 -0300

    -

--- PROMO GNEWS.js
+++ PROMO GNEWS.js
@@ -27,6 +27,14 @@ function PROMO_GNEWS_UTL(thisObj) {
   #include 'source/layout/main ui.js'; // → UI definition file
   /* jshint ignore:end */
 
+  // loads all preferences...
+  loadStaticPrefs();
+  loadDefaultPrefs();
+  
+  // writes on the 'info' window panel...
+  clearOutput();
+  write(aboutStr);
+  
   var PROMO_GNEWS_WINDOW = PROMO_GNEWS_UI(thisObj);
 
   // checks network access...
